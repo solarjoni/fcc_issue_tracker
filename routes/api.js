cd4bad8c18f51caac01094422d6ebe8e07e6c022
solarjoni <solarjoni@gmail.com>
2022-04-26 20:38:19 +0300

    mongo and mongoose

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { assignNewMochaID } = require("mocha/lib/utils");
+
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
@@ -11,7 +13,17 @@ module.exports = function (app) {
     
     .post(function (req, res){
       let project = req.params.project;
-      
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project
+      })
     })
     
     .put(function (req, res){
