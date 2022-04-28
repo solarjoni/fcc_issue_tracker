eb63f0d7aa37f42101ca0f50a1e385490b063aec
solarjoni <solarjoni@gmail.com>
2022-04-28 20:39:19 +0300

    mongodb set up

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,31 @@
 'use strict';
 
 const { assignNewMochaID } = require("mocha/lib/utils");
+let mongodb = require('mongodb')
+let mongoose = require('mongoose')
+
+let uri = 'mongodb+srv://new-user:' + process.env.PW + '@cluster0.2cqnt.mongodb.net/fcc_issue_tracker?retryWrites=true&w=majority'
 
 module.exports = function (app) {
 
-  app.route('/api/issues/:project')
   
+  mongoose.connect(uri, { useNewUrlPArser: true, useUnifiedTopology: true, serverApi: mongodb.ServerApiVersion.v1} )
+  
+  let issueSchema = new mongoose.Schema({
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_by: {type: String, required: true},
+    assigned_to: String,
+    status_text: String,
+    open: {type: Boolean, required: true},
+    created_on: {type: Date, required: true},
+    updated_on: {type: Date, required: true},
+    project: String
+  })
+
+  let Issue = mongoose.model('issue', issueSchema)
+  
+  app.route('/api/issues/:project')
     .get(function (req, res){
       let project = req.params.project;
       
